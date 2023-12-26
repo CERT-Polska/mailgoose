195beed6ee3e4e7737c41484ced7a2772f7077c3
Krzysztof Zając <krzysztof.zajac@cert.pl>
2023-12-26 08:33:16 +0100

    lint

--- docs/conf.py
+++ docs/conf.py
@@ -9,6 +9,7 @@
 import datetime
 import os
 import sys
+from typing import List
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -69,7 +70,7 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = []
+html_static_path: List[str] = []
 
 
 html_css_files = [
