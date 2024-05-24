7bf4cd05cbfb2bf66cc822b95a761ac51df50340
KarolisJasadKK <38495063+KarolisJasad@users.noreply.github.com>
2024-05-24 11:41:43 +0300

    Lt translations (#66)
    Co-authored-by: Krzysztof Zając <krzysztof.zajac2@gmail.com>

--- scan/libmailgoose/translate.py
+++ scan/libmailgoose/translate.py
@@ -74,6 +74,10 @@ TRANSLATIONS = {
             "DMARC politika yra 'none', o 'rua' nėra nustatytas, tai reiškia, kad DMARC nustatymas nėra veiksmingas.",
         ),
         (
+            "DMARC subdomain policy is 'none' and 'rua' is not set, which means that the DMARC setting is not effective for subdomains.",
+            "DMARC subdomeno poliika yra 'none', o 'rua' nėra nustatytas, tai reiškia, kad DMARC nustatymas nėra veiksmingas subdomenams.",
+        ),
+        (
             f"The DMARC record must be located at {PLACEHOLDER}, not {PLACEHOLDER}",
             f"DMARC įrašas turi būti įdiegtas į {PLACEHOLDER}, o ne {PLACEHOLDER}.",
         ),
@@ -149,6 +153,10 @@ TRANSLATIONS = {
             "siųsti didelį kiekį el. laiškų naudojant įvairias priemones, o neteisingo pranešimo pristatymas "
             "yra nepriimtinas. Tokiais atvejais raportai turėtų būti atidžiai stebimi, o nustatyti nustatymai "
             "turėtų būti 'quarantine' arba 'reject'.",
+        ),
+        (
+            "DMARC subdomain policy is 'none', which means that besides reporting no action will be taken for e-mails coming from subdomains. ",
+            "DMARC subdomeno poliika yra 'none', tai reiškia, kad DMARC nustatymas nėra veiksmingas subdomenams.",
         ),
         (
             "rua tag (destination for aggregate reports) not found",
