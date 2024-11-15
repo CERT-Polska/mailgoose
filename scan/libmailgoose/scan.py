9a00ec0c112c04ca33fe1bfdf5162198052aff33
Krzysztof Zając <krzysztof.zajac2@gmail.com>
2024-11-15 15:29:42 +0100

    italian ham for linter

--- scan/libmailgoose/scan.py
+++ scan/libmailgoose/scan.py
@@ -339,7 +339,7 @@ def scan_domain(
         # According to Section 6.6.1 of RFC7489 (https://datatracker.ietf.org/doc/html/rfc7489):
         # If the domain is encoded with UTF-8, the domain name must be converted to an A-label, as described
         # in Section 2.3 of [IDNA], for further processing.
-        from_domain = from_domain.encode('idna').decode('ascii')
+        from_domain = from_domain.encode("idna").decode("ascii")
 
         try:
             dmarc_query = checkdmarc.dmarc.query_dmarc_record(from_domain, nameservers=nameservers, timeout=timeout)
