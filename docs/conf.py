c94328d96827c9e3c27cf0212d0720aeb3eb87ac
Krzysztof Zając <krzysztof.zajac2@gmail.com>
2024-12-11 15:10:06 +0100

    Version bumps

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ copyright = f"{datetime.datetime.now().year}, CERT Polska"
 author = "CERT Polska"
 
 # The full version, including alpha/beta/rc tags
-release = "1.3.4"
+release = "1.3.5"
 
 latex_engine = "xelatex"
 
