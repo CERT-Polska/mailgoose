db3a0f6ba44b2c5746927c9c20de47b590990b86
kazet <krzysztof.zajac@cert.pl>
2025-06-16 09:16:20 +0200

    Not creating two warnings when one is needed (#140)

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ copyright = f"{datetime.datetime.now().year}, CERT Polska"
 author = "CERT Polska"
 
 # The full version, including alpha/beta/rc tags
-release = "1.3.7"
+release = "1.3.8"
 
 latex_engine = "xelatex"
 
