0723fbecb58d9f0bb9689ca8c4e2486e6058a8d5
Krzysztof Zając <krzysztof.zajac@cert.pl>
2026-05-19 11:40:21 +0200

    .

--- test/test_ssl.py
+++ test/test_ssl.py
@@ -4,6 +4,7 @@ from base import BaseTestCase
 class SSLTestCase(BaseTestCase):
     def test_dmarc_starts_with_whitespace(self) -> None:
         result = self.check_domain_api_v1("mailserver")
+        self.assertEqual(result, {})
         del result["result"]["timestamp"]
         self.maxDiff = None
         self.assertEqual(result, {})
