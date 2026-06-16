c9416f434cfe97c110ad6afa6bf7e467caa301eb
snicket2100 <57048005+snicket2100@users.noreply.github.com>
2026-06-16 13:53:35 +0200

    Using a temporary directory to save the compiled `messages.mo` files
    Fixes race conditon exception when multiple workers try to write and
    then read the same file.

--- app/src/templates.py
+++ app/src/templates.py
@@ -1,5 +1,7 @@
 import gettext
+import os
 import subprocess
+import tempfile
 
 from fastapi.templating import Jinja2Templates
 
@@ -10,21 +12,25 @@ from .template_utils import mailgoose_urlize
 def setup_templates(language: str) -> Jinja2Templates:
     templates = Jinja2Templates(directory="templates", extensions=[BuildIDTag, LanguageTag, "jinja2.ext.i18n"])
     templates.env.filters["mailgoose_urlize"] = mailgoose_urlize
-    subprocess.call(
-        [
-            "pybabel",
-            "compile",
-            "-f",
-            "--input",
-            f"/app/translations/{language}/LC_MESSAGES/messages.po",
-            "--output",
-            f"/app/translations/{language}/LC_MESSAGES/messages.mo",
-        ],
-        stderr=subprocess.DEVNULL,  # suppress a misleading message where compiled translations will be saved
-    )
 
-    templates.env.install_gettext_translations(  # type: ignore
-        gettext.translation(domain="messages", localedir="/app/translations", languages=[language]), newstyle=True
-    )
+    with tempfile.TemporaryDirectory() as localedir:
+        output_dir = os.path.join(localedir, language, "LC_MESSAGES")
+        os.makedirs(output_dir)
+        subprocess.call(
+            [
+                "pybabel",
+                "compile",
+                "-f",
+                "--input",
+                f"/app/translations/{language}/LC_MESSAGES/messages.po",
+                "--output",
+                os.path.join(output_dir, "messages.mo"),
+            ],
+            stderr=subprocess.DEVNULL,  # suppress a misleading message where compiled translations will be saved
+        )
+
+        templates.env.install_gettext_translations(  # type: ignore
+            gettext.translation(domain="messages", localedir=localedir, languages=[language]), newstyle=True
+        )
 
     return templates
